9adeee463a5afabd8daf38164a6f76be08a35538
Jamil Omar <jfalconi63@hotmail.com>
2021-04-15 16:53:09 -0400

    feat: gitignore

--- api/index.ts
+++ api/index.ts
@@ -4,7 +4,7 @@ import * as path from 'path';
 import os = require('os');
 import SocketIOServer = require('socket.io');
 const app = express();
-const port = 9000; // default port to listen
+const port = 8080; // default port to listen
 const uiAppUrl = path.join(__dirname, '../../build');
 app.use(express.static(uiAppUrl));
 const expressServer = app.listen(port);
